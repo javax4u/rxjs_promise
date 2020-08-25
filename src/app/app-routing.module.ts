4148ce6e889437681b702b48c0e7131d27784319
rupesh kumar <javax4u@gmail.com>
2020-08-25 17:00:09 +0530

    observable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'promise', component:PromiseComponent},
+  {path:'**',redirectTo:'promise'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromiseComponent } from './promise/promise.component';
+import { ObservableComponent } from './observable/observable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromiseComponent,
+    ObservableComponent
   ],
   imports: [
     BrowserModule,
